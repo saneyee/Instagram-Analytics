a8d778ddc767bcd470716112387a6b9bbefe1144
Saneyee Sarkar <saneyeesarkar@gmail.com>
2018-03-30 00:19:14 -0700

    the header shows the user info section

--- src/Home/Header/Header.jsx
+++ src/Home/Header/Header.jsx
@@ -1,42 +1,42 @@
 import React, {Component} from 'react';
 import { Button } from 'react-bootstrap';
-import './index.css';
 
+import './index.css';
 class Header extends Component {
     handleLoginClick = () => {
         window.location = this.props.loginURL;
     }
 
-
     render() {
         const userInfo = this.props.userInfo;
-        return(
+        return (
             <div className="header">
                 <div className="col-md-2 logo">
                     Analytica
                 </div>
-
-                <div className="col-md-8 user-info">
-                        <img className="profile-pic" src={userInfo.profile_picture} width="50" height="50" alt="userMedia"/>
+                {
+                    (userInfo !==null) &&
+                    <div className="col-md-8 user-info">
+                        <img className="profile-pic" src={userInfo.profile_picture} width="50" height="50" alt="uerMedia"/>
                         <span> {userInfo.full_name} | Follwers : {userInfo.counts.followed_by} | Following: {userInfo.counts.follows} | Media {userInfo.counts.media}</span>
-                </div>
+                    </div>
+                }
+                {
+                    (userInfo === null) &&
+                    <div className="col-md-2 col-md-offset-8 login">
+                        <Button bsStyle="primary" onClick={() => this.handleLoginClick()}>Login</Button>
+                    </div>
+                }
+                {
+                    (userInfo !==null) &&
+                    <div className="col-md-2 log-out">
+                        <Button bsStyle="primary" onClick={() => this.props.handleLogout()}>Logout</Button>
+                    </div>
+                }
 
-                <div className="col-md-2 col-md-offset-8 login">
-                    <Button bsStyle="primary" onClick={() => this.handleLoginClick()} >Login</Button>
-                </div>
-
-
-                <div className="col-md-2 log-out">
-                        <Button bsStyle="primary" onClick={() => this.handleLoginClick()}>Logout</Button>
-                </div>
             </div>
         );
     }
-
-
-
-
-
 }
 
 export default Header;
---
+++ src/Home/Home.jsx
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import Header from './Header/Header.jsx';
+import MostLikedPost from './PageItems/MostLikedPost.jsx';
+import MostCommentedPost from './PageItems/MostCommentedPost.jsx';
+import TagSpread from './PageItems/TagSpread.jsx';
+import Landing from './PageItems/Landing.jsx';
+class Home extends Component {
+    render() {
+        const {...props} = this.props;
+        return (
+            <div className="page-wrapper">
+                <Header {...props} />
+                {
+                    this.props.userMedia &&
+                    <div className="page-elements">
+                        <MostLikedPost userMedia= {this.props.userMedia} />
+                        <MostCommentedPost userMedia= {this.props.userMedia} />
+                        <TagSpread userMedia= {this.props.userMedia} />
+                    </div>
+                }
+                {
+                    !this.props.userMedia && !window.localStorage.access_token &&
+                    <Landing />
+                }
+
+            </div>
+        );
+    }
+}
+
+export default Home;
