4acd5cd4aba37ae68d237f575e2b8d9dea3ce322
Saneyee Sarkar <saneyeesarkar@gmail.com>
2018-03-29 23:32:38 -0700

    adds user info and styles in the document

--- src/Home/Header/Header.jsx
+++ src/Home/Header/Header.jsx
@@ -15,6 +15,11 @@ class Header extends Component {
                     Analytica
                 </div>
 
+                <div className="col-md-8 user-info">
+                        <img className="profile-pic" src={userInfo.profile_picture} width="50" height="50" alt="uerMedia"/>
+                        <span> {userInfo.full_name} | Follwers : {userInfo.counts.followed_by} | Following: {userInfo.counts.follows} | Media {userInfo.counts.media}</span>
+                </div>
+
                 <div className="col-md-2 col-md-offset-8 login">
                     <Button bsStyle="primary" onClick={() => this.handleLoginClick()} >Login</Button>
                 </div>
